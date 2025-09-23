b2e4e35ca7d447941f858032841dfb532ff2b874
JosueYS <ysabraham633gmail.com>
2025-09-23 08:27:49 -0500

    puerto render

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 // Render asigna el puerto automáticamente
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1000;
 
 // Middlewares
 app.use(cors());
