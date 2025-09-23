ab61aaabdbc6ca2c3b745d286fd3a71fef4983ea
JosueYS <ysabraham633gmail.com>
2025-09-23 10:09:05 -0500

    cambio en la conexion de la db

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -8,12 +8,15 @@ const connection = mysql.createConnection({
   port: process.env.DB_PORT || 3306
 });
 
-connection.connect(err => {
+// Conexión de prueba
+pool.getConnection((err, conn) => {
   if (err) {
-    console.error('Error de conexión:', err);
-    return;
+    console.error('❌ Error de conexión:', err);
+  } else {
+    console.log('✅ Conectado a la base de datos MySQL');
+    conn.release();
   }
-  console.log('Conectado a la base de datos MySQL');
 });
 
-module.exports = connection;
+// Exportar con soporte de Promesas
+module.exports = pool.promise();
